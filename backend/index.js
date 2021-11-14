9a4eb6338cf3d7eacf3df2c2fb30d149f9d864ac
Damian Wasilewski <230032@edu.p.lodz.pl>
2021-11-14 13:22:49 +0100

    Add backend registration

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
 
+const routersUrls = require("./routes/routes");
+
+dotenv.config();
+
+mongoose.connect(process.env.DB_URL, () => console.log("Database connected"));
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(helmet());
+
+app.use("/api", routersUrls);
 app.listen(5000, () => {
   console.log("Server is running");
 });
---
+++ backend/routes/routes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+
+const UserSchema = require("../models/UserSchema");
+
+router.post("/register", async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  const securePassword = await bcrypt.hash(req.body.password, salt);
+
+  const registerUser = new UserSchema({
+    fullName: req.body.fullName,
+    username: req.body.username,
+    email: req.body.email,
+    password: securePassword,
+  });
+  registerUser
+    .save()
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
+});
+
+module.exports = router;
