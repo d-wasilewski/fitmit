b73db936c784b7e6cbd71c661543ec8e49b6cdd4
Michał <michal.tosik@wp.pl>
2021-11-25 01:41:59 +0100

    Es

--- client/App.js
+++ client/App.js
@@ -28,19 +28,7 @@ export default function App() {
   });
 
   if (!loaded) return null;
-
-  const config = {
-    animation: 'spring',
-    config: {
-      stiffness: 1000,
-      damping: 50,
-      mass: 3,
-      overshootClamping: false,
-      restDisplacementThreshold: 0.01,
-      restSpeedThreshold: 0.01,
-    },
-  }
-
+  
   return (
     <NavigationContainer>
       <StatusBar hidden={false} animated={true} translucent={false} />
