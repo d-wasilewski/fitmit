c626a3629a6ae6080f8e306e63c0dc935c086e0e
Filip Michalski <fidzzip@gmail.com>
2021-12-06 15:34:51 +0100

    EventSection.js done

--- client/components/homepage/EventSection.js
+++ client/components/homepage/EventSection.js
@@ -1,31 +1,27 @@
 import React from "react";
-import {
-  View,
-  StyleSheet,
-  ImageBackground,
-  Dimensions,
-  Text,
-} from "react-native";
+import { View, StyleSheet, ImageBackground, Text } from "react-native";
 import EventSlider from "./upcoming_events/EventSlider";
 import bgImg from "../../assets/greenwavy.png";
 import colors from "../../styles/colors";
 
 const EventSection = (props) => {
-  const windowWidth = Dimensions.get("window").width;
-
   const styles = StyleSheet.create({
     container: {
+      marginTop: "5%",
+      marginBottom: "60%",
       flex: 1,
       flexDirection: "column",
       justifyContent: "space-between",
+      overflow: "visible",
+      position: "relative",
     },
     headerText: {
       fontFamily: "ComfortaaBold",
       fontSize: 20,
       color: colors.white,
     },
-    headerWrapper: {},
-    slider: { position: "relative", top: 100 },
+    headerWrapper: { padding: "7%", paddingTop: "14%" },
+    slider: { position: "absolute", top: "60%", left: 0 },
   });
 
   return (
@@ -33,13 +29,13 @@ const EventSection = (props) => {
       <ImageBackground
         source={bgImg}
         resizeMode="cover"
-        style={{ width: "100%" }}
+        style={{ width: "100%", minHeight: 150 }}
       >
         <View style={styles.headerWrapper}>
           <Text style={styles.headerText}>Upcoming Events</Text>
         </View>
-        <EventSlider style={styles.slider}></EventSlider>
       </ImageBackground>
+      <EventSlider style={styles.slider}></EventSlider>
     </View>
   );
 };
