b4f3cb4e7f08af11f41686025a8ab6d80a2187cc
Damian Wasilewski <230032@edu.p.lodz.pl>
2021-12-14 11:26:47 +0100

    Logout user after the token expires

--- client/App.js
+++ client/App.js
@@ -1,10 +1,12 @@
-import React from "react";
-import { StyleSheet, Text, View, StatusBar } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StatusBar, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useFonts } from "expo-font";
 import { Provider } from "react-redux";
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import jwtDecode from "jwt-decode";
 
 import Start from "./pages/start";
 import Login from "./pages/login";
@@ -12,12 +14,38 @@ import Register from "./pages/register";
 import Home from "./pages/home";
 import User from "./pages/userProfile";
 import store from "./redux/store";
+import { loginUser, logoutUser } from "./redux/actions/userActions";
 
 const Stack = createNativeStackNavigator();
 
 axios.defaults.baseURL = "http://192.168.1.11:5000/api/";
 
 export default function App() {
+  const [tok, setTok] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [routeName, setRouteName] = useState("Login");
+
+  useEffect(async () => {
+    const tok = await AsyncStorage.getItem("authToken");
+
+    if (tok) {
+      const decodedToken = jwtDecode(tok);
+      // console.log("Token : ", decodedToken);
+      if (decodedToken.exp * 1000 < Date.now()) {
+        store.dispatch(logoutUser());
+        // console.log("wylogowywuje");
+        setRouteName("Login");
+      } else {
+        axios.defaults.headers.common["Authorization"] = tok;
+        // FIXME: zmienic loginUser na SET_AUTHENTICATED i GET_USER/{id_z_tokenu}
+        store.dispatch(loginUser({ login: "User", password: "12345" }));
+        setRouteName("Home");
+        // console.log("loguje");
+      }
+    }
+    setLoading(false);
+  }, [tok, loading, routeName]);
+
   const [loaded] = useFonts({
     RobotoRegular: require("./assets/fonts/Roboto-Regular.ttf"),
     RobotoBlack: require("./assets/fonts/Roboto-Black.ttf"),
@@ -41,22 +69,26 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <StatusBar hidden={false} animated={true} translucent={false} />
-        <Stack.Navigator
-          initialRouteName="Login"
-          screenOptions={{
-            headerShown: false,
-            animation: "slide_from_right",
-          }}
-        >
-          <Stack.Screen name="Start" component={Start} />
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Register" component={Register} />
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="User" component={User} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      {!loading ? (
+        <NavigationContainer>
+          <StatusBar hidden={false} animated={true} translucent={false} />
+          <Stack.Navigator
+            initialRouteName={routeName}
+            screenOptions={{
+              headerShown: false,
+              animation: "slide_from_right",
+            }}
+          >
+            <Stack.Screen name="Start" component={Start} />
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Register" component={Register} />
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="User" component={User} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      ) : (
+        <Text>Loading...</Text>
+      )}
     </Provider>
   );
 }
